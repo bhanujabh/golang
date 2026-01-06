6d8db251f4377e381e07e936b877c0def233989f
Bhanuja Bhardwaj <bhardwajbhanuja@gmail.com>
2026-01-06 11:19:15 +0530

    updated variadic functions code

--- 13_variadic_functions/variadic.go
+++ 13_variadic_functions/variadic.go
@@ -2,7 +2,11 @@ package main
 
 import "fmt"
 
+// func sum(nums ...interface{}) int {
+// interface is used to take any type of parameters 
 func sum(nums ...int) int {
+	// in the parameter above we got a slice of nums 
+	// using ... we can receive multiple parameters 
 	total := 0
 
 	for _, num := range nums {
@@ -13,7 +17,11 @@ func sum(nums ...int) int {
 }
 
 func main() {
+	fmt.Println(1, 2, 3, 4, 5, 86, "hello") // 1 2 3 4 5 86 hello in single line 
+	// Println() can receive any number of parameters 
+	// hence it is called a variadic function 
 	nums := []int{3, 4, 5, 6}
-	result := sum(nums...)
-	fmt.Println(result)
+	// result := sum(3, 4, 5, 6)
+	result := sum(nums...)// ... spread operator 
+	fmt.Println(result) // 18
 }
